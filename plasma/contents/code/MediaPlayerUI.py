717a024259be46a4270f46bb7300f14abe57e0ad
M0Rf30 <morfeo89@hotmail.it>
2014-03-16 18:26:59 +0100

    Removed unused imports

--- plasma/contents/code/MediaPlayerUI.py
+++ plasma/contents/code/MediaPlayerUI.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import dbus,  os, datetime
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
