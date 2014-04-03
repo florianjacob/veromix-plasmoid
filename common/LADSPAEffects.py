b6f4d4ca21c87d571bce27cea654a2ab027d4af0
M0Rf30 <morfeo89@hotmail.it>
2014-04-03 16:47:40 +0200

    conf dir for LADSPA blacklist

--- common/LADSPAEffects.py
+++ common/LADSPAEffects.py
@@ -168,7 +168,9 @@ class LADSPAPresetLoader:
 
 _effects = None
 class LADSPAEffects:
-    blacklist_file = os.getenv('HOME') + "/.pulse/veromix-ladspa-blacklist.conf"
+    configpath = os.getenv('HOME') + ".veromix"
+    assure_path_exists(configpath)
+    blacklist_file = os.getenv('HOME') + ".veromix/veromix-ladspa-blacklist.conf"
 
     def effects(self, do_reload=False):
         global _effects
@@ -243,6 +245,11 @@ def fetch_plugins():
     if len(plugins) == 0:
         return hardcoded_plugins()
     return plugins
+
+def assure_path_exists(path):
+  dir = os.path.dirname(path)
+  if not os.path.exists(dir):
+  os.makedir(dir)
 
 def extract_plugin(filename, lines):
     definition = {
@@ -405,90 +412,6 @@ def hardcoded_plugins():
             "range" : [[1, 8], [10, 40], [0, 2], [0, 5], [2, 30], [-20, 0]],
             "scale" : [1,10,10,10, 10, 10 ],
             "labels" : ["Voices", "Delay base", "Voice separation", "Detune", "LFO frequency", "Output attenuation"] } ]
-
-        ## GOOD
-        #sink_name="sink_name=ladspa_output.dj_eq_1901.dj_eq."+str(self.ladspa_index)
-        #plugin = "plugin=dj_eq_1901"
-        #label = "label=dj_eq_mono"
-        #control = "control=0,0,0"
-
-        # fun!
-        #sink_name="sink_name=ladspa_output.multivoice_chorus_1201.multivoiceChorus."+str(self.ladspa_index)
-        #plugin = "plugin=multivoice_chorus_1201"
-        #label = "label=multivoiceChorus"
-        #control = "control=0,0,0,0,0,0"
-
-        ## fun
-        #sink_name="sink_name=ladspa_output.pitch_scale_1193.pitchScale."+str(self.ladspa_index)
-        #plugin = "plugin=pitch_scale_1193"
-        #label = "label=pitchScale"
-        #control = "control=1.9"
-
-        ##works but ..
-        #sink_name="sink_name=ladspa_output.flanger_1191.flanger."+str(self.ladspa_index)
-        #plugin = "plugin=flanger_1191"
-        #label = "label=flanger"
-        #control = "control=0,0,0,0"
-
-        ## not working?
-        #sink_name="sink_name=ladspa_output.df_flanger_1438.djFlanger."+str(self.ladspa_index)
-        #plugin = "plugin=dj_flanger_1438"
-        #label = "label=djFlanger"
-        #control = "control=0,0,0,0"
-
-        ## ..
-        #sink_name="sink_name=ladspa_output.phasers_1217.autoPhaser."+str(self.ladspa_index)
-        #plugin = "plugin=phasers_1217"
-        #label = "label=autoPhaser"
-        #control = "control=0,0,0,0,0"
-
-        ## does not work
-        #sink_name="sink_name=ladspa_output.dj_eq_1901.dj_eq."+str(self.ladspa_index)
-        #plugin = "plugin=dj_eq_1901"
-        #label = "label=dj_eq"
-        #control = "control=0,0,0"
-
-        ## no
-        #sink_name="sink_name=ladspa_output.decay_1886.decay."+str(self.ladspa_index)
-        #plugin = "plugin=decay_1886"
-        #label = "label=decay"
-        #control = "control=0"
-
-        ## i dont hear it
-        #sink_name="sink_name=ladspa_output.delay_1898.delay_n."+str(self.ladspa_index)
-        #plugin = "plugin=delay_1898"
-        #label = "label=delay_n"
-        #control = "control=0,0"
-
-        ## i dont hear it
-        #sink_name="sink_name=ladspa_output.delay_1898.delay_l."+str(self.ladspa_index)
-        #plugin = "plugin=delay_1898"
-        #label = "label=delay_l"
-        #control = "control=0,0"
-
-        ## i dont hear it
-        #sink_name="sink_name=ladspa_output.delay_1898.delay_c."+str(self.ladspa_index)
-        #plugin = "plugin=delay_1898"
-        #label = "label=delay_c"
-        #control = "control=0,0"
-
-        ## does not work (stereo)
-        #sink_name="sink_name=ladspa_output.karaoke_1409.karaoke."+str(self.ladspa_index)
-        #plugin = "plugin=karaoke_1409"
-        #label = "label=karaoke"
-        #control = "control=-50"
-
-        ## not work (stereo)
-        #sink_name="sink_name=ladspa_output.plate_1423.plate."+str(self.ladspa_index)
-        #plugin = "plugin=plate_1423"
-        #label = "label=plate"
-        #control = "control=0,0,0"
-
-        ## less fun
-        #sink_name="sink_name=ladspa_output.pitch_scale_1194.pitchScaleHQ."+str(self.ladspa_index)
-        #plugin = "plugin=pitch_scale_1194"
-        #label = "label=pitchScaleHQ"
-        #control = "control=1.9"
 
 
 if __name__ == '__main__':
